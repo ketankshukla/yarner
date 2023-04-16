e2e23136029e63a328dacb59401704672d9ae5ac
Ketan Shukla <ketankshukla@gmail.com>
2023-04-16 05:34:24 -0700

    changed to ES6 imports

--- math.test.js
+++ math.test.js
@@ -1,17 +1,17 @@
-const arithmetic = require("./math");
+import { add, subtract, multiply, divide } from "./math";
 
 test("Add 2 + 1 to equal 3", () => {
-  expect(arithmetic.add(2, 1)).toBe(3);
+  expect(add(2, 1)).toBe(3);
 });
 
 test("Subtract 2 - 1 to equal 1", () => {
-    expect(arithmetic.subtract(2, 1)).toBe(1);
+    expect(subtract(2, 1)).toBe(1);
   });
 
 test("Multiply 2 * 9 to equal 18", () => {
-    expect(arithmetic.multiply(2, 9)).toBe(18);
+    expect(multiply(2, 9)).toBe(18);
   });
 
 test("Divide 100 / 5 to equal 20", () => {
-    expect(arithmetic.divide(100, 5)).toBe(20);
+    expect(divide(100, 5)).toBe(20);
   });
